1e8af22712a1f6bdc893e712c60f87954a6a095e
DAPRO2012 <DAPRO987654@gmail.com>
2025-10-27 22:33:06 +0000

    Update __init__.py

--- src/quickDash/__init__.py
+++ src/quickDash/__init__.py
@@ -17,8 +17,16 @@ class DashBoard:
             pass
 
     def run(self, host: str=None, port: int=None, debug: bool=None):
-        if not self.kwargs.get("staticFolder", "static") in os.listdir(os.getcwd()):
-            os.makedirs(os.path.join(os.getcwd(), self.kwargs.get("staticFolder", "static")))
-        if not self.kwargs.get("staticFolder", "static")+"/css" in os.listdir(os.getcwd()+"/static"):
-            shutil.copyfile(__file__+"/../templateFiles/static/style.css", os.path.join(os.getcwd(), self.kwargs.get("staticFolder", "static"), "css", "style.css"))
+        #If static folder doesnt exist then create
+        if not self.kwargs.get("staticFolder", "static") in os.listdir(os.path.join(__file__, "..")):
+            os.makedirs(os.path.join(__file__, "..", self.kwargs.get("staticFolder", "static")))
+        #If css in static folder does not exist then create
+        if not "css" in os.listdir(os.path.join(__file__, "..", self.kwargs.get("staticFolder", "static"))):
+            os.mkdir(os.path.join(__file__, "..", self.kwargs.get("staticFolder", "static"), "css"))
+        # if the main styling is not copied then copy
+        if not "style.css" in os.listdir(os.path.join(__file__, "..", self.kwargs.get("staticFolder", "static"), "css")):
+            shutil.copyfile(os.path.join(__file__, "..", "templateFiles", "static", "style.css"), os.path.join(os.getcwd(), self.kwargs.get("staticFolder", "static"), "css", "style.css"))
+        if not self.kwargs.get("templatesFolder", "templates") in os.listdir(os.path.join(__file__, "..", self.kwargs.get("templatesFolder", "templates"))):
+            shutil.copyfile(os.path.join(__file__, "..", "templateFiles", "static", "style.css"), os.path.join(os.getcwd(), self.kwargs.get("staticFolder", "static"), "css", "style.css"))
+        self._app.run(host, port, debug)
         self._app.run(host, port, debug)
