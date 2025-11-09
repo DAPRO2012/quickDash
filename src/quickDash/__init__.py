efeae83ae50e2205b70625555734293a1466e1a6
dapro2012 <bean@beancake.co.uk>
2025-11-09 12:43:38 +0000

    modified:   src/quickDash/__init__.py 	renamed:    src/quickDash/templateFiles/static/homeIcon.svg -> src/quickDash/static/homeIcon.svg 	renamed:    src/quickDash/templateFiles/static/onClick.js -> src/quickDash/static/onClick.js 	renamed:    src/quickDash/templateFiles/static/style.css -> src/quickDash/static/style.css 	deleted:    src/quickDash/templateFiles/templates/dashMainPage.html 	renamed:    src/quickDash/templateFiles/templates/masterDash.html -> src/quickDash/templates/master.html

--- src/quickDash/__init__.py
+++ src/quickDash/__init__.py
@@ -1,32 +1,26 @@
 from flask import Flask, render_template, session
+from collections.abc import Callable
 import os
-import shutil
 
-class DashBoard:
-    def __init__(self, importName, name="Dashboard", indexName="Index", secretKey: bytes = "--unsecure-secretkey--", **kwargs):
-        self._app = Flask(importName, static_folder="templateFiles/static", template_folder="templateFiles/templates")
-        self._app.secret_key = secretKey
-        self.kwargs = kwargs
-        @self._app.route("/")
-        def index():
-            return render_template("dashMainPage.html", name=name+" | "+indexName, theme=session.get("theme", "light"))
-        
-        def addPage(route: str, pageName: str):
-            #@self._app.route(route)
-            #def createPage():
-            pass
+class Dashboard():
+    def __init__(self, module_name, title="Dashboard") -> None:
+        self.app = Flask(module_name)
+        self.title = title
+        self.theme = "light"
+        self.run = self.app.run
+    
 
-    def run(self, host: str=None, port: int=None, debug: bool=None):
-        #If static folder doesnt exist then create
-        if not self.kwargs.get("staticFolder", "static") in os.listdir(os.path.join(__file__, "..")):
-            os.makedirs(os.path.join(__file__, "..", self.kwargs.get("staticFolder", "static")))
-        #If css in static folder does not exist then create
-        if not "css" in os.listdir(os.path.join(__file__, "..", self.kwargs.get("staticFolder", "static"))):
-            os.mkdir(os.path.join(__file__, "..", self.kwargs.get("staticFolder", "static"), "css"))
-        # if the main styling is not copied then copy
-        if not "style.css" in os.listdir(os.path.join(__file__, "..", self.kwargs.get("staticFolder", "static"), "css")):
-            shutil.copyfile(os.path.join(__file__, "..", "templateFiles", "static", "style.css"), os.path.join(os.getcwd(), self.kwargs.get("staticFolder", "static"), "css", "style.css"))
-        if not self.kwargs.get("templatesFolder", "templates") in os.listdir(os.path.join(__file__, "..", self.kwargs.get("templatesFolder", "templates"))):
-            shutil.copyfile(os.path.join(__file__, "..", "templateFiles", "static", "style.css"), os.path.join(os.getcwd(), self.kwargs.get("staticFolder", "static"), "css", "style.css"))
-        self._app.run(host, port, debug)
-        self._app.run(host, port, debug)
+class Page():
+    def __init__(self, dashboard: Dashboard, url: str, title: str) -> None:
+        self.dashboard = dashboard
+        self.title = title
+        self.content = ""
+        def mkPage():
+            return render_template("master.html", title=f"{self.dashboard.title} | {self.title}", content = self.content, theme = self.dashboard.theme)
+        mkPage.__name__ = self.title
+        self.dashboard.app.add_url_rule(url, self.title, mkPage)
+
+dash = Dashboard(__name__)
+page1 = Page(dash, "/", "page1")
+
+dash.run(debug=True)
