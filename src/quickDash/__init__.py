b3cdf32f132fd217b70beb566539e091bcc4793b
dapro2012 <bean@beancake.co.uk>
2025-11-13 21:57:05 +0000

    modified:   src/quickDash/__init__.py 	modified:   src/quickDash/__pycache__/data_representation.cpython-313.pyc 	modified:   src/quickDash/data_representation.py 	modified:   src/quickDash/static/onClick.js 	modified:   src/quickDash/static/style.css 	modified:   src/quickDash/templates/master.html

--- src/quickDash/__init__.py
+++ src/quickDash/__init__.py
@@ -1,6 +1,9 @@
 import flask
 from typing import Callable
 import os
+import math
+
+request = flask.request
 
 class Dashboard():
     def __init__(self, module_name, title="Dashboard") -> None:
@@ -12,22 +15,35 @@ class Dashboard():
     def route(self, rule: str, **kwargs):
         def decorator(f: Callable):
             def mkPage():
-                return flask.render_template("master.html", title=f.__name__, content=f(), theme=self.theme)
+                request = flask.request
+                return flask.render_template("master.html", title=f"{self.title} | "+f.__name__, content=f"<h1>{f.__name__}</h1>"+f(), theme=self.theme)
             self.app.add_url_rule(rule, f.__name__, mkPage, None, **kwargs)
             return f
         return decorator
 
-import data_representation
+from data_representation import data, color
 import random
 
 dash = Dashboard(__name__)
 
+def area_of_circle(radius: int):
+    return int(radius*radius*math.pi)
+def volume_of_cone(radius: int, height: int):
+    return int(radius*radius*height*math.pi/3)
+def volume_of_cylinder(radius: int, height: int):
+    return int(radius*radius*math.pi*height)
+
+def areas_using_radius(radius: int = 2, height: int = 2):
+    return [area_of_circle(radius), volume_of_cone(radius, height), volume_of_cylinder(radius, height)]
+
 @dash.route("/", methods=["GET", "POST"])
 def Index():
-    if flask.request.method == "POST":
-        testdata = data_representation.data("Something", [int(flask.request.form.get("int1", "0")), int(flask.request.form.get("int2", "0"))])
-    else:
-        testdata = data_representation.data("Something", [1, 1])
-    return data_representation.form("form1", int1=int, int2=int)+testdata.to_bar_chart()
+    html = ""
+    bar_chart = data("Coffees Sold", [(64, "Jan"), (55, "Feb"), (42, "Mar"), (29, "Apr"), (38, "May"), (34, "Jun"), (74, "Jul"), (52, "Aug"), (54, "Sep"), (62, "Oct"), (75, "Nov"), (89, "Dec")])
+    pie_chart = data("Earth's Atmosphere", [(78, "Nitrogen"), (21, "Oxygen"), (1, "Other")])
+
+    html += bar_chart.to_bar_chart([color(0, 128, 0), color("#FE620B"), color("#1A7DF2")])
+    html += pie_chart.to_pie_chart([color(0, 128, 0), color("#FE620B"), color("#1A7DF2")])
+    return html
 
 dash.run(debug=True)
--- src/quickDash/data_representation.py
+++ src/quickDash/data_representation.py
@@ -1,5 +1,6 @@
 from typing import overload, Union, Optional
 from itertools import cycle, islice
+import flask
 
 class color:
     r: int
@@ -33,13 +34,21 @@ class color:
 
 
 class coordinates:
-    def __init__(self, data_name: str | None = None, values: list[tuple[int, int]] | None = None, accents: list[color] | None = None) -> None:
+    values: list[tuple[int, int]]
+
+    def __init__(self, data_name: str, values: list[tuple[int, int]]) -> None:
+        self.data_name = data_name
+        self.values = values
+
+    def as_time_graph(self, accents: list[color] = [color(0, 128, 0)], height: int = 500, width: int = 100) -> str:
+        #TODO: make time graph
         ...
+
 
 class data:
     values: list[int | tuple[int, str]]
 
-    def __init__(self, data_name: str | None = None, values: list[int | tuple[int, str]] = []) -> None:
+    def __init__(self, data_name: str, values: list[int | tuple[int, str]]) -> None:
         self.data_name = data_name
         self.values = values
 
@@ -48,32 +57,55 @@ class data:
 
     def to_bar_chart(self, accents: list[color] = [color(0, 128, 0)], height: int = 500, width: int = 100) -> str:
         html=""
-        maxVal = 1
+        maxVal = 0
         accent_cycle = cycle(accents)
         accents_adjusted = list(islice(accent_cycle, len(self.values)))
-        print(accents_adjusted)
         for index, value in enumerate(self.values):
-            print(index)
             if isinstance(value, tuple):
                 if value[0] > maxVal:
                     maxVal = value[0]
-                html += f"<div class='bar' style='background-color: {accents_adjusted[index]};'>{"<br>"*value[0]}{value[1]}</div>"
+                html += f"<div class='bar' style='background-color: {accents_adjusted[index]};' text='{value[1]}'>{"<br>"*value[0]}</div>"
             else:
                 if value > maxVal:
                     maxVal = value
-                html += f"<div class='bar' style='background-color: {accents_adjusted[index]};'>{"<br>"*(value-1)}{value}</div>"
-        html = f"<div class='horizontal-bar-chart' style='line-height: {height/maxVal}px; width: calc( {width}% - 10px - 7px - {len(self.values)*2}px );' title='{self.data_name}'>"+html
-        html += "</div>"
+                html += f"<div class='bar' style='background-color: {accents_adjusted[index]};' text='{value}'>{"<br>"*value}</div>"
+        print(maxVal)
+        html = f"<fieldset class='horizontal-bar-chart' style='line-height: {height/maxVal if maxVal > 0 else 30}px; height: {height}; width: calc( {width}% - 10px - 7px - {len(self.values)*2}px );' title='{self.data_name}'><legend>{self.data_name}</legend>{html}</fieldset>"
+        return html
+    def to_pie_chart(self, accents: list[color] = [color(128, 0, 0), color(0, 128, 0), color(0, 0, 128)], diameter: int = 200) -> str:
+        accent_cycle = cycle(accents)
+        accents_adjusted = list(islice(accent_cycle, len(self.values)))
+        sum_of_values = 0
+        for value in self.values:
+            if isinstance(value, tuple):
+                sum_of_values += value[0]
+            else:
+                sum_of_values += value
+        deg_value = sum_of_values/100
+        prev_values = 0
+        gradient = ""
+        legend = ""
+        for index, value in enumerate(self.values):
+            if isinstance(value, tuple):
+                prev_values += value[0]
+                legend += f"<div style='background-color: {accents_adjusted[index]}; display: inline-block; border-radius: 5px; width: 25px; height: 25px; transform: translateY(6.25px)'></div>  {value[1]} - {value[0]}%<br>"
+            else:
+                prev_values += value
+            gradient += f"{accents_adjusted[index]} 0 {deg_value*(prev_values)}%, "
+        html = f"<fieldset class='pie-chart-wrapper'><legend>{self.data_name}</legend><div class='pie-chart' style='height: {diameter}px; width: {diameter}px; background-image: conic-gradient({gradient.removesuffix(", ")});'></div><br>{legend.removesuffix("<br>")}</fieldset>"
         return html
     
-def form(form_id: str, **inputs: type) -> str:
-    html = f"<hr><form id='{form_id}' method='post' onchange='document.getElementById(\"{form_id}\").submit()'>"
+def form(form_id: str, auto_submit: bool = False, **inputs: type) -> str:
+    html = f"<hr><form id='{form_id}' method='post' onchange='document.getElementById(\"{form_id if auto_submit else None}\").submit()'>"
     for input in inputs:
         input_type = inputs.get(input)
+        value = None
+        if input in flask.request.form:
+            value = flask.request.form.get(input)
         if input_type == str:
-            html += f"<input type='text' name='{input}' placeholder='{input}'>"
+            html += f"<input type='text' name='{input}' placeholder='{input}' value='{value}'>"
         if input_type == int:
-            html += f"<input type='number' name='{input}' placeholder='{input}'>"
+            html += f"<input type='number' name='{input}' placeholder='{input}' value='{value}'>"
     html += "</form>"
 
     return html
